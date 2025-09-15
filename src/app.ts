6f2169bf7b1d6e8be4b1b110da232963b93a68a9
Gabriel Conceição <gabrielc2019@outlook.com.br>
2025-09-15 15:35:13 -0300

    feat: add prisma

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,11 @@
 import fastify from 'fastify'
+import { PrismaClient } from 'generated/prisma'
 export const app = fastify()
+
+const prisma = new PrismaClient()
+prisma.user.create({
+  data: {
+    name: 'John Doe',
+    email: '5tH5w@example.com',
+  },
+})
