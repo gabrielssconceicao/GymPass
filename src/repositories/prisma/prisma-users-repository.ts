04a8a3e31eb446267d8cf7299d29a2d424e17c95
Gabriel Conceição <gabrielc2019@outlook.com.br>
2025-09-16 16:00:09 -0300

    feat: create user repository interface

---
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -0,0 +1,24 @@
+import { Prisma } from 'generated/prisma'
+
+import { prisma } from '@/lib/prisma'
+
+import { UsersRepository } from '../users-repository'
+export class PrismaUsersRepository implements UsersRepository {
+  async create(data: Prisma.UserCreateInput) {
+    const user = await prisma.user.create({
+      data,
+    })
+
+    return user
+  }
+
+  async findByEmail(email: string) {
+    const userWithSameEmail = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    return userWithSameEmail
+  }
+}
