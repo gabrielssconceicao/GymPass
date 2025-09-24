541236d11e071b7bb424514aef163391cf37088d
Gabriel Conceição <gabrielc2019@outlook.com.br>
2025-09-24 15:34:06 -0300

    feat: add gyms repositories

---
+++ src/repositories/prisma/prisma-gyms-repository.ts
@@ -0,0 +1,55 @@
+import { Gym, Prisma } from 'generated/prisma'
+
+import { prisma } from '@/lib/prisma'
+
+import { FindManyNearByParams, GymsRepository } from '../gyms-repository'
+
+export class PrismaGymsRepository implements GymsRepository {
+  async create(data: Prisma.GymCreateInput) {
+    const gym = await prisma.gym.create({
+      data,
+    })
+
+    return gym
+  }
+  async findById(id: string) {
+    const gym = await prisma.gym.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    return gym
+  }
+  async searchMany(query: string, page: number) {
+    const gyms = await prisma.gym.findMany({
+      where: {
+        title: {
+          contains: query,
+        },
+      },
+      take: 20,
+      skip: (page - 1) * 20,
+    })
+
+    return gyms
+  }
+
+  async findManyNearBy({
+    latitude,
+    longitude,
+  }: FindManyNearByParams): Promise<Gym[]> {
+    const gyms = await prisma.$queryRaw<Gym[]>`
+      SELECT * FROM gyms
+      WHERE 6371 * acos(
+        cos(radians(${latitude})) 
+        * cos(radians(latitude)) 
+        * cos(radians(longitude) - radians(${longitude})) 
+        + sin(radians(${latitude})) 
+        * sin(radians(latitude))
+      ) <= 10
+    `
+
+    return gyms
+  }
+}
