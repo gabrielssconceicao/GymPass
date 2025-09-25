1e33cc965a417bad1d746bc70704ab75c6980de8
Gabriel Conceição <gabrielc2019@outlook.com.br>
2025-09-25 12:02:10 -0300

    feat: get profile controller and jwt middleware

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,7 +1,20 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 
+import { makeGetUserProfileUseCase } from '@/use-cases/factories/make-get-user-profile-use-case'
+
 export async function profile(req: FastifyRequest, rep: FastifyReply) {
-  await req.jwtVerify()
-  console.log(req.user.sub)
-  return rep.status(200).send()
+  // await req.jwtVerify()
+
+  const getUserProfile = makeGetUserProfileUseCase()
+
+  const { user } = await getUserProfile.execute({
+    userId: req.user.sub,
+  })
+
+  return rep.status(200).send({
+    user: {
+      ...user,
+      password_hash: undefined,
+    },
+  })
 }
