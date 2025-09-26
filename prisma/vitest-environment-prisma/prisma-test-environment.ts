accb476c4e8d61576a291a589fca7ac31dee0e2c
Gabriel Conceição <gabrielc2019@outlook.com.br>
2025-09-26 18:02:01 -0300

    feat: create an new schema for each test

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,4 +1,25 @@
+import 'dotenv/config'
+
+import { execSync } from 'node:child_process'
+import { randomUUID } from 'node:crypto'
+
+import { PrismaClient } from 'generated/prisma'
 import type { Environment } from 'vitest/environments'
+
+let prisma: PrismaClient
+
+// it's a good idea to use a different schema for each test
+function generateDatabaseUrl(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provide a DATABASE_URL environment variable.')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
+}
 
 export default <Environment>{
   name: 'prisma',
@@ -6,11 +27,25 @@ export default <Environment>{
 
   async setup() {
     // custom setup
-    console.log('Setup')
+    const schema = randomUUID()
+    const databaseUrl = generateDatabaseUrl(schema)
+
+    // it'll set the DATABASE_URL environment variable for each test
+    process.env.DATABASE_URL = databaseUrl
+
+    // run migrations
+    execSync('npx prisma db push', {
+      stdio: 'inherit',
+    })
+    prisma = new PrismaClient()
+
     return {
       async teardown() {
-        // called after all tests with this env have been run
-        console.log('Teardown')
+        // clean up
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+        )
+        await prisma.$disconnect()
       },
     }
   },
